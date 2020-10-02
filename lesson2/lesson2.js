d6c2debc1c9f0e09b9ae5154daed41ecd100e0ee
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-10-01 19:47:49 -0600

    push

--- lesson2/lesson2.js
+++
@@ -1,9 +0,0 @@
-window.onload = updateBox;
-
-function updateBox(){
-    let lastUpdate = "Last Updated: " + document.lastModified;
-    let date = new Date().getFullYear();
-
-    document.getElementById('year').innerHTML = date;
-    document.getElementById('updated').innerHTML = lastUpdate;
-}
