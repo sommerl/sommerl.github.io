9e7ee22b6914d1e8413fa0ada09ef5d379c94c45
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-10-12 16:52:44 -0600

    push

--- lesson4/js/file.js
+++ lesson4/js/file.js
@@ -1,13 +1,13 @@
 window.onload = updateBox;
 
 function updateBox(){
-    let date= new Date() .getFullYear();
+    let date= new Date().getFullYear();
     let d= new Date();
-    let Monthday = d.getDate();
+    let monthDay = d.getDate();
     let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    document.getElementById('updated').innerHTML = days [d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' ' + date;
+    document.getElementById('updated').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' ' + date;
 }
 
 function toggleMenu(){
