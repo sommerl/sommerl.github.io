4690639fdefb8a6bbc4c20d3608aa9e4c34a4fe2
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-10-12 16:56:20 -0600

    push

--- lesson4/js/file.js
+++ lesson4/js/file.js
@@ -7,7 +7,7 @@ function updateBox(){
     let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    document.getElementById('updated').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' ' + date;
+    document.getElementById('updated').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' '+ date;
 }
 
 function toggleMenu(){
