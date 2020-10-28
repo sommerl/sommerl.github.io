878223b0a44a0651964ceafbda55fc252d7e6a2e
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-10-28 17:25:47 -0600

    push

--- lesson7/lazyload.js
+++ lesson7/lazyload.js
@@ -1,18 +1,30 @@
-const imagesToLoad = document.querySelectorAll("img[data-scr]");
+const images = document.querySelectorAll("[data-scr]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-scr");
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
+}
 
 const imgOptions = {
     threshold: 0,
     rootMargin: "0px, 0px, 50px, 0px"
 };
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image.removeAttribute('data-src');};
-};
-
-if('IntercsectionObserver' in window) {
-    const imageObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-        }
+const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+        return;
+    } else {
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
     }
-}
+});
+}, imgOptions);
+
+imgages.forEach(image => {
+    imgObserver.observe(image);
+});
