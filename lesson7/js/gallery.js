10dc66b4b0cfe3d3be457497b9a5579ccef6f303
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-10-29 18:11:18 -0600

    push

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -1,56 +1,33 @@
-const pictures = document.querySelectorAll('.boxbox');
 
-function preloadPicture(picture) {
-    const images = picture.querySelectorAll('[data-src]');
-    const sources =picture.querySelectorAll('[data-srcset]')
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-    images.forEach(img => {
-        preloadImage(img);
-     })
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
 
-     sources.forEach(source => {
-         preloadSource(source);
-     })
-}
+const loadImages = (image) => {
+ image.setAttribute('src', image.getAttribute('data-src'));
+ image.onLoad = () => {image.removeAttribute('data-src');};
+};
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src){
-        return;
-    }
-    img.src = src;
-    img.removeAttribute('data-src');
-}
-
-function preloadSource(source) {
-    const srcset = source.getAttribute("data-srcset");
-    if (!srcset){
-        return;
-    }
-
-    const picOptions = {
-        threshold: 0,
-        rootMargin: "0px 0px 50px 0px"
-    };
-
-    source.srcset = srcset;
-    source.removeAttribute('data-srcset')
-}
-
-const picObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadPicture(entry.target);
-            imgObserver.unobserve(entry.target);
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
         }
-    })
-}, picOptions);
-
-pictures.forEach(picture => {
-    picObserver.observe(picture);
-})
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
 
   window.onload = updateBox;
 
