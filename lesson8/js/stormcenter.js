9e150e99049432c1ce9d1f87843c384007cc87cc
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-04 21:27:33 -0700

    push

--- lesson8/js/stormcenter.js
+++ lesson8/js/stormcenter.js
@@ -9,3 +9,26 @@ function selectResponse() {
 	s.textContent = sel.value;
 	
 }
+
+window.onload = updateBox;
+
+function updateBox(){
+    let date= new Date().getFullYear();
+    let d= new Date();
+    let monthDay = d.getDate();
+    let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    document.getElementById('updated').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' '+ date;
+}
+
+function toggleMenu(){
+    document.getElementById("mainNav").classList.toggle("hide");
+}
+
+var dayOfWeek = new Date().getDay() // This gives us a number (1-7)
+
+if (dayOfWeek == 5) {
+    document.getElementById("big-banner").style.display = "block";
+    }
+    
