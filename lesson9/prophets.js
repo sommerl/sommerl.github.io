60015edbe953a04d0be8a4843b4a016ea53571cb
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-07 18:13:26 -0700

    push

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,6 +1,5 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-const prophets = jsonObject['prophets'];
 
 fetch(requestURL)
   .then(function (response) {
@@ -11,13 +10,4 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {}
   });
 
-  let card = document.createElement('section');
-let h2 = document.createElement('h2');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
-
-  
