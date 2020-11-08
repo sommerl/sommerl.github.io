7e927b1ee417bc1dbcc8070de23f6d9530444a22
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-07 18:06:36 -0700

    push

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,5 +1,7 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
+const prophets = jsonObject['prophets'];
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
@@ -7,11 +9,7 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing 
     for (let i = 0; i < prophets.length; i++ ) {
-    const prophets = jsonObject['prophets'];
   });
-
-
-  const prophets = jsonObject['prophets'];
 
   let card = document.createElement('section');
 let h2 = document.createElement('h2');
