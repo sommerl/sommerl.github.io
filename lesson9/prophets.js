896696ecc957cf0e233fe22caaeb01a41fb2408d
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-09 17:34:48 -0700

    push

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -7,16 +7,16 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    for (let i = 0; i < prophets.length; i++ ) {prophets}
+    console.table(jsonObject); 
+ // temporary checking for valid response and data parsing
+ for (let i = 0; i < prophets.length; i++ ) {
+   let card = document.createElement('section');
+ let h2 = document.createElement('h2');
+ 
+ h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+ 
+ card.appendChild(h2);
+ 
+ document.querySelector('div.cards').appendChild(card);}
   });
 
-  
-  let card = document.createElement('section');
-  let h2 = document.createElement('h2');
-  
-  h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-  
-  card.appendChild(h2);
-  
-  document.querySelector('div.cards').appendChild(card);
