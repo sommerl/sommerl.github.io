44a360549dd5396eb7966a1430fddec05033889b
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-10 10:23:23 -0700

    push

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,19 +6,17 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject); 
- // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
  for (let i = 0; i < prophets.length; i++ ) {
    let card = document.createElement('section');
- let h2 = document.createElement('h2');
+   let h2 = document.createElement('h2');
  
  h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
- 
+ image.setAttribute('src', prophets[i].imageurl);
+
  card.appendChild(h2);
  
  document.querySelector('div.cards').appendChild(card);}
   });
 
-  image.setAttribute('src', prophets[i].imageurl);
-
-
-  const prophets = jsonObject['prophets'];
+  
