970021f88beee523dcf9f1dabf29c16a856ba3a9
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-09 17:03:38 -0700

    push

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -6,8 +6,5 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing 
-    for (let i = 0; i < prophets.length; i++ ) {}
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
   });
-
-
