dc25384eb67070b3bc0c281d01879624bc4509cc
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-09 22:13:48 -0700

    push

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -10,7 +10,6 @@ fetch(requestURL)
  for (let i = 0; i < prophets.length; i++ ) {
    let card = document.createElement('section');
  let h2 = document.createElement('h2');
- const prophets = jsonObject['prophets'];
  
  h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
  
@@ -22,4 +21,4 @@ fetch(requestURL)
   image.setAttribute('src', prophets[i].imageurl);
 
 
-
+  const prophets = jsonObject['prophets'];
