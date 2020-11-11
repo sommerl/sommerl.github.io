2b26451fcab251a79e670be4292f27cacf955feb
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-11 11:30:20 -0700

    push

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -9,29 +9,29 @@ fetch(requestURL)
     const towns = jsonObject['towns'];
     for (let i = 0; i < towns.length; i++ ) {
         let town = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let image = document.createElement('img');
-      let motto = document.createElement('p');
-      let yF = document.createElement('p');
-      let cP = document.createElement('p');
-      let aR = document.createElement('p');
+        let h2 = document.createElement('h2');
+        let image = document.createElement('img');
+        let motto = document.createElement('p');
+        let yF = document.createElement('p');
+        let cP = document.createElement('p');
+        let aR = document.createElement('p');
 
       
-      h2.textContent = towns[i].name;
-      image.setAttribute('src', towns[i].photo);
-      motto.textContent = "Motto: " + towns[i].motto;
-      yF.textContent = "Year Founded: " + towns[i].yearFounded;
-      cP.textContent = "Current Population: " + towns[i].currentPopulation;
-      aR.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+        h2.textContent = towns[i].name;
+        image.setAttribute('src', towns[i].photo);
+        motto.textContent = "Motto: " + towns[i].motto;
+        yF.textContent = "Year Founded: " + towns[i].yearFounded;
+        cP.textContent = "Current Population: " + towns[i].currentPopulation;
+        aR.textContent = "Average Rainfall: " + towns[i].averageRainfall;
       
       
-      town.appendChild(h2);
-      town.appendChild(image);
-      town.appendChild(yF);
-      town.appendChild(cP);
-      town.appendChild(aR);
-      town.appendChild(motto);
+        town.appendChild(h2);
+        town.appendChild(image);
+        town.appendChild(yF);
+        town.appendChild(cP);
+        town.appendChild(aR);
+        town.appendChild(motto);
       
-      document.querySelector('div.towninfo').appendChild(town);}
-       });
+        document.querySelector('div.towninfo').appendChild(town);}
+        });
      
