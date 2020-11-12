97a63ae0102826eb3f739e44c4048951e9f16339
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-12 14:46:23 -0700

    push

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -19,19 +19,19 @@ fetch(requestURL)
 
 
             h2.textContent = towns[i].name;
+            motto.textContent = towns[i].motto;
             image.setAttribute('src', `images/${towns[i].photo}`);
-            motto.textContent = "Motto: " + towns[i].motto;
             yF.textContent = "Year Founded: " + towns[i].yearFounded;
             cP.textContent = "Current Population: " + towns[i].currentPopulation;
             aR.textContent = "Average Rainfall: " + towns[i].averageRainfall;
 
 
             town.appendChild(h2);
+            town.appendChild(motto);
             town.appendChild(image);
             town.appendChild(yF);
             town.appendChild(cP);
             town.appendChild(aR);
-            town.appendChild(motto);
 
             document.querySelector('div.towninfo').appendChild(town);
         }
