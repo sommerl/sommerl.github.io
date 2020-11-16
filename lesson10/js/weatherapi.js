068e19615ccf9856652b42fe3c97e9cf42633ed4
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-16 13:28:22 -0700

    push

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,11 +1,8 @@
 const apiURL = "api.openweathermap.org/data/2.5/weather?id={5604473}&appid={4d6384241cfb8e06721808d523039337}";
 
 fetch(apiURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.log(jsonObject);
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
   });
-
 
