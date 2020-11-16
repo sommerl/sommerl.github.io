3ecfd2015d7e76a619c95c5029a254fa6f852715
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-16 13:54:17 -0700

    push

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=290e5c7b68ca4a803da90608515efd48";
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=290e5c7b68ca4a803da90608515efd48";
 
 fetch(apiURL)
     .then((response) => response.json())
