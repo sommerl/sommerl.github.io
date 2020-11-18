e1fee061504ed2399d7d492a1c24b00d979064df
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-17 17:21:37 -0700

    push

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,7 +4,7 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById(' current-temp').textContent = jsObject.main.temp;
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
         const desc = jsObject.weather[0].description;  
         document.getElementById('imagesrc').textContent = imagesrc;  
