b694afa743bb9205bf0768abb4f3b689ae649bc8
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-11-18 09:12:20 -0700

    push

--- lesson10/js/preston10.js
+++ lesson10/js/preston10.js
@@ -16,3 +16,22 @@ fetch(apiURL)
         document.getElementById('icon').setAttribute('alt', desc);
     });
 
+    const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=290e5c7b68ca4a803da90608515efd48";
+
+    fetch(forecastURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.log(jsonObject);
+        const forecast = jsonObject['forecast'];
+        for (let i = 0; i < forecast; i++) {
+        document.getElementById('monday').textContent = jsObject.main.temp;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+        const desc = jsObject.weather[0].description;  
+        document.getElementById('imagesrc').textContent = imagesrc;  
+        document.getElementById('icon').setAttribute('src', imagesrc); 
+        document.getElementById('icon').setAttribute('alt', desc);
+        }
+    });
+
