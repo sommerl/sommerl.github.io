a90ec8f40ae250411101c8ba27d75aa35fc8f1e3
Laura Sommer <lauralosommer@Lauras-MacBook-Air.local>
2020-12-03 20:23:26 -0700

    push

---
+++ scoots/js/file.js
@@ -0,0 +1,9 @@
+let lastmod = document.lastModified;
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+document.getElementById("lastupdated").innerHTML = "Last Updated: " + lastmod;
+
+function toggleMenu(){
+    document.getElementById("mainNav").classList.toggle("hide");
+}
